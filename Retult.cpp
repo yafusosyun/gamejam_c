80e1c3cb59b94471a1dde9740a17e48fdb371028
kanekumasanosuke <122763496+kanekumasanosuke@users.noreply.github.com>
2023-06-05 17:26:40 +0900

    変更なし

--- Retult.cpp
+++ Retult.cpp
@@ -6,6 +6,11 @@ int page = 0;		//ページ移動
 //エンド描画
 void DrawResult(int key, int& gamemode, int score)
 {
+	//リザルト画像表示
+	//DrawGraph(0, 0, g_Img, FALSE);
+
+	static int menuNo = 0;
+
 	//リザルト画面から選択画面に移動する用
 	if (key & PAD_INPUT_4) {
 		page = 1;
