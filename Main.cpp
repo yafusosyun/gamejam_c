10b32a142a4c9129dcc5606284ae2c313e890069
gao2968 <118335646+gao2968@users.noreply.github.com>
2023-06-08 13:33:46 +0900

    修正

--- Main.cpp
+++ Main.cpp
@@ -2,13 +2,6 @@
 #include "SceneManager.h"
 #include "PadInput.h"
 #include "fps.h"
-#include "HammerAnimation.h"
-#include "UI.h"
-#include "GameMain.h"
-#include "nail.h"
-#include "InputRankingScene.h"
-#include "DrawRankingScene.h"
-#include "Ranking.h"
 #include "Title.h"
 
 #define _SCREEN_HEIGHT_ 720
@@ -39,14 +32,7 @@ int WINAPI WinMain(_In_ HINSTANCE ih, _In_opt_ HINSTANCE ioh, _In_ LPSTR il, _In
 		return -1;
 	}
 
-
-
 	SceneManager sceneMng(dynamic_cast<AbstractScene*>(new Title())); 
-
-
-	//SceneManager sceneMng(/*new Title*/);
-
-	HammerAnimation::AnimInit();
 
 	while (sceneMng.Update() != nullptr && ProcessMessage() != -1 && !PadInput::OnClick(XINPUT_BUTTON_BACK))
 	{
